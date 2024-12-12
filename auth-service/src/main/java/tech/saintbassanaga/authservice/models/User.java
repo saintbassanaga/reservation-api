b096d863bf800436d7503fa32329d134b590558e
saintbassanaga <saintbassanaga01@gmail.com>
2024-12-12 17:20:17 +0100

    chore: Adding Config Server with Spring Cloud
    To Init the project Repository
    
    Author: saintbassanaga

--- auth-service/src/main/java/tech/saintbassanaga/authservice/models/User.java
+++ auth-service/src/main/java/tech/saintbassanaga/authservice/models/User.java
@@ -1,6 +1,7 @@
 package tech.saintbassanaga.authservice.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,7 +11,9 @@ import tech.saintbassanaga.authservice.models.mapped.Credential;
 @Getter
 @Setter
 @Entity
-@Table(name = "users")
+@Table(name = "users" , indexes = {
+        @Index(name = "user_uuid_username_index", columnList = "uuid ,username")
+})
 public class User extends AuditingEntity {
     private Civility civility;
     private Credential credential;
